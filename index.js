75c77b1d5a81fad5fc5695a2ba9712e4931da636
MatiAlva <83080975+MatiAlva@users.noreply.github.com>
2023-02-13 16:57:31 -0300

    :white_check_mark: Path * agregado

--- index.js
+++ index.js
@@ -22,6 +22,13 @@ app.use(express.json())
 //Rutas
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/events', require('./routes/events'))
+
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+
 //TODO: CRUD: Eventos
 
 // Escuchar peticiones
